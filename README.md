dbc105bca6a9af05087f00d721fcd948d87dede3
Jônatas Pereira <jonataspereiradev@gmail.com>
2023-12-20 23:14:17 -0300

    Update files

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # jogo-da-memoria
+
+## Autor
+Jônatas Pereira da Rocha
+
+## Regras do jogo
+O objetivo do jogo da memória é encontrar pares de cartas idênticas. Um conjunto de cartas com pares idênticos (você pode usar cartas com imagens ou fazer suas próprias cartas).
+
+## Como jogar
+
+
+## Como executar:
+
+
+## Créditos
+Para a alaboração do projeto, foi utilizado por referência referência 
+## Licença
+
+Este projeto é licenciado sob a Licença MIT - consulte o arquivo [LICENSE](LICENSE) para obter detalhes.
--- js/scripts.js
+++ js/scripts.js
@@ -1,126 +1,183 @@
-function start() {
-    document.addEventListener("DOMContentLoaded", () => {
-        const cards = [
-            {
-                name: "cometa",
-                img: "images/cometa.jpg"
-            },
-            {
-                name: "foguete",
-                img: "images/foguete.jpg"
-            },
-            {
-                name: "nave",
-                img: "images/nave.jpg"
-            },
-            {
-                name: "nave2",
-                img: "images/nave2.jpg"
-            },
-            {
-                name: "planetab",
-                img: "images/planetab.jpg"
-            },
-            {
-                name: "planetal",
-                img: "images/planetal.jpg"
-            },
-            {
-                name: "sol",
-                img: "images/sol.jpg"
-            },
-            {
-                name: "terra",
-                img: "images/terra.jpg"
-            },
-            {
-                name: "cometa",
-                img: "images/cometa.jpg"
-            },
-            {
-                name: "foguete",
-                img: "images/foguete.jpg"
-            },
-            {
-                name: "nave",
-                img: "images/nave.jpg"
-            },
-            {
-                name: "nave2",
-                img: "images/nave2.jpg"
-            },
-            {
-                name: "planetab",
-                img: "images/planetab.jpg"
-            },
-            {
-                name: "planetal",
-                img: "images/planetal.jpg"
-            },
-            {
-                name: "sol",
-                img: "images/sol.jpg"
-            },
-            {
-                name: "terra",
-                img: "images/terra.jpg"
-            }
-        ];
-    
-        cards.sort(() => 0.5 - Math.random());
-        const board = document.querySelector('.board');
-        const resultView = document.querySelector('.result');
-        let cardsChosen = [];
-        let cardsChosenId = [];
-        let cardsWon = [];
-    
-        function creatBoard() {
-            for(let i = 0; i < cards.length; i++) {
-                const card = document.createElement('img');
-                card.setAttribute('src', 'images/board.jpg');
-                card.setAttribute('data-id', i);
-                card.addEventListener('click', flipCard);
-                board.appendChild(card);
-            }
+const initialDispaly = document.querySelector('.initialDisplay')
+const board = document.querySelector('.board')
+let temporizador
+let tempoRestante = 60
+let nome
+let pontuacao
+
+document.addEventListener("DOMContentLoaded", () => {
+    const cards = [
+        {
+            name: "cometa",
+            img: "images/cometa.jpg"
+        },
+        {
+            name: "foguete",
+            img: "images/foguete.jpg"
+        },
+        {
+            name: "nave",
+            img: "images/nave.jpg"
+        },
+        {
+            name: "nave2",
+            img: "images/nave2.jpg"
+        },
+        {
+            name: "planetab",
+            img: "images/planetab.jpg"
+        },
+        {
+            name: "planetal",
+            img: "images/planetal.jpg"
+        },
+        {
+            name: "sol",
+            img: "images/sol.jpg"
+        },
+        {
+            name: "terra",
+            img: "images/terra.jpg"
+        },
+        {
+            name: "cometa",
+            img: "images/cometa.jpg"
+        },
+        {
+            name: "foguete",
+            img: "images/foguete.jpg"
+        },
+        {
+            name: "nave",
+            img: "images/nave.jpg"
+        },
+        {
+            name: "nave2",
+            img: "images/nave2.jpg"
+        },
+        {
+            name: "planetab",
+            img: "images/planetab.jpg"
+        },
+        {
+            name: "planetal",
+            img: "images/planetal.jpg"
+        },
+        {
+            name: "sol",
+            img: "images/sol.jpg"
+        },
+        {
+            name: "terra",
+            img: "images/terra.jpg"
         }
-    
-        function checkForMatch() {
-            const cards = document.querySelectorAll('img')
-            const optionOneId = cardsChosenId[0]
-            const optionTwoId = cardsChosenId[1]
-    
-            if(optionOneId == optionTwoId) {
-              cards[optionOneId].setAttribute('src', 'images/board.jpg')
-              cards[optionTwoId].setAttribute('src', 'images/board.jpg')
-            } else if(cardsChosen[0] === cardsChosen[1]) {
-              cards[optionOneId].setAttribute('src', 'images/check.png')
-              cards[optionTwoId].setAttribute('src', 'images/check.png')
-              cards[optionOneId].removeEventListener('click', flipCard)
-              cards[optionTwoId].removeEventListener('click', flipCard)
-              cardsWon.push(cardsChosen)
-            } else {
-              cards[optionOneId].setAttribute('src', 'images/board.jpg')
-              cards[optionTwoId].setAttribute('src', 'images/board.jpg')
-            }
-            cardsChosen = []
-            cardsChosenId = []
-            resultView.textContent = `Pares Encontrados: ${cardsWon.length}`;
-            if (cardsWon.length === cards.length / 2) {
-                resultView.textContent = 'Parabéns! Você conseguiu encontrar todas as cartas'
-            }
-          }
-    
-        function flipCard() {
-            let cardId = this.getAttribute('data-id')
-            cardsChosen.push(cards[cardId].name)
-            cardsChosenId.push(cardId)
-            this.setAttribute('src', cards[cardId].img)
-            if (cardsChosen.length ===2) {
-              setTimeout(checkForMatch, 500)
-            }
+    ]
+
+    cards.sort(() => 0.5 - Math.random())
+    const painel = document.querySelector('.painel')
+    const pares = document.querySelector('.pares')
+    const pontos = document.querySelector('.pontos')
+    let cardsChosen = []
+    let cardsChosenId = []
+    let cardsWon = []
+
+    document.getElementById("btn-entrar").addEventListener("click", function(e) {
+        const name = document.querySelector(".nome").value
+        if(name.trim() === "") {
+            alert("O nome não pode ficar em branco!")
+            e.preventDefault() 
+        } else {
+            initialDispaly.style.display = "none"
+            nome = name
+            e.preventDefault()  
         }
-        creatBoard();
-    });
+        startDisplay()
+    })
+
+    function startDisplay() {
+        const novaDiv = document.createElement('div')
+        novaDiv.className = "startDisplay"
+        const novoParagrafo = document.createElement('p')
+        const texto = document.createTextNode(`Seja bem-vindo, ${nome}! Espero que se divirta muito...`)
+        const button = document.createElement('button')
+        button.textContent = "Começar"
+        novoParagrafo.appendChild(texto)
+        novaDiv.appendChild(novoParagrafo)
+        novaDiv.appendChild(button)
+        document.body.appendChild(novaDiv)
+        button.addEventListener("click", creatBoard)
+    }
+
+    function endGame() {
+        document.body.appendChild(document.createElement("div"))
+    }
+
+    function creatBoard() {
+        document.querySelector('.startDisplay').style.visibility = 'hidden'
+        painel.style.visibility = 'visible'
+        iniciarTemporizador()
+        for(let i = 0; i < cards.length; i++) {
+            const card = document.createElement('img')
+            card.setAttribute('src', 'images/board.jpg')
+            card.setAttribute('data-id', i)
+            card.addEventListener('click', flipCard)
+            board.appendChild(card)
+        }
+    }
+
+    function checkForMatch() {
+        const cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+
+        if(optionOneId == optionTwoId) {
+            cards[optionOneId].setAttribute('src', 'images/board.jpg')
+            cards[optionTwoId].setAttribute('src', 'images/board.jpg')
+        } else if(cardsChosen[0] === cardsChosen[1]) {
+            cards[optionOneId].setAttribute('src', 'images/checksf.png')
+            cards[optionTwoId].setAttribute('src', 'images/checksf.png')
+            cards[optionOneId].removeEventListener('click', flipCard)
+            cards[optionTwoId].removeEventListener('click', flipCard)
+            cardsWon.push(cardsChosen)
+        } else {
+            cards[optionOneId].setAttribute('src', 'images/board.jpg')
+            cards[optionTwoId].setAttribute('src', 'images/board.jpg')
+        }
+
+        cardsChosen = []
+        cardsChosenId = []
+
+        pares.textContent = `Pares encontrados: ${cardsWon.length}`
+        pontos.textContent = `Pontuação: ${cardsWon.length}`
+        
+        if(cardsWon.length === cards.length / 2) {
+        }
+    }
+
+    function flipCard() {
+        let cardId = this.getAttribute('data-id')
+        cardsChosen.push(cards[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src', cards[cardId].img)
+        if(cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 300)
+        }
+    }
+})
+
+function iniciarTemporizador() {
+    temporizador = setInterval(function() {
+      tempoRestante--
+      document.getElementById("tempoRestante").innerText = tempoRestante
+
+      if (tempoRestante <= 0) {
+        alert("Tempo esgotado.!")
+        reiniciarTemporizador()
+      }
+    }, 1000)
 }
 
-start();
+function reiniciarTemporizador() {
+    tempoRestante = 60
+    document.getElementById("tempoRestante").innerText = tempoRestante
+}
